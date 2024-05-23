eab0547b2662e86af9247385c7699f11eef16c27
LY <1481813890@qq.com>
2024-05-23 19:55:29 +0800

    home-goodsItem组件封装

--- src/api/home.js
+++ src/api/home.js
@@ -7,15 +7,21 @@ export function getBannerAPI() {
     url: 'home/banner'
   })
 }
-
+// 获取新鲜好物
 export function getNewAPI() {
   return httpInstance({
     url: '/home/new'
   })
 }
-
+// 获取人气推荐
 export function getHotAPI() {
   return httpInstance({
     url: 'home/hot'
+  })
+}
+//获取所有商品模块
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: '/home/goods'
   })
 }
