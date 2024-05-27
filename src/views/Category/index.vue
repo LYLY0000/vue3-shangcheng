b843b9027a768cf06a13998bacfe98415931bab2
LY <1481813890@qq.com>
2024-05-27 21:52:37 +0800

    路由缓存问题解决

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,16 +2,22 @@
 import { getCategoryAPI } from "@/api/category";
 import { getBannerAPI } from "@/api/home";
 import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
+import { onBeforeRouteUpdate, useRoute } from "vue-router";
 import GoodsItem from "../Home/components/Goodsitem.vue";
 //获取数据
 const categoryData = ref({});
 const route = useRoute(); //获取参数
-const getCategory = async () => {
-  const res = await getCategoryAPI(route.params.id);
+const getCategory = async (id = route.params.id) => {
+  const res = await getCategoryAPI(id);
   categoryData.value = res.result;
 };
 onMounted(() => getCategory());
+//目标：路由参数变化的时候 可以把分类数据接口重新发送
+onBeforeRouteUpdate((to) => {
+  //存在问题：使用最新的路由参数请求最新的分类数据
+  //解决方法：利用onBeforeRouteUpdate所给的to数据传参
+  getCategory(to.params.id);
+});
 //获取Banner
 let bannerList = ref([]);
 const getBanner = async () => {
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,24 +1,24 @@
 <script setup>
-import LayoutNav from './components/LayoutNav.vue'
-import LayoutHeader from './components/LayoutHeader.vue'
-import LayoutFooter from './components/LayoutFooter.vue'
-import LayoutFixed from './components/LayoutFixed.vue'
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+import LayoutFixed from "./components/LayoutFixed.vue";
 
 //触发获取导航列表的action
 
-import { useCategoryStore } from '@/stores/category'
-import { onMounted } from 'vue'
+import { useCategoryStore } from "@/stores/category";
+import { onMounted } from "vue";
 
-const categoryStory=useCategoryStore()
+const categoryStory = useCategoryStore();
 
-onMounted(()=>categoryStory.getCategory())
-
+onMounted(() => categoryStory.getCategory());
 </script>
 
 <template>
-  <LayoutFixed/>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
+  <!-- <RouterView :key="$route.fullPath" /> -->
   <RouterView />
   <LayoutFooter />
 </template>
