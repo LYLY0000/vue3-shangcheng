2f7f4e3adde1a9e42d62cda50475862811566617
LY <1481813890@qq.com>
2024-06-12 22:04:10 +0800

    图片预览组件-props适配

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -42,7 +42,7 @@ onMounted(() => getGoods());
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <imageView></imageView>
+              <imageView :image-list="goods.mainPictures"></imageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
