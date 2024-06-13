726397ceff618ce7a6da403710b79d3be625cbbe
LY <1481813890@qq.com>
2024-06-13 11:39:13 +0800

    登录-表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,20 @@
 <script setup>
+//表单校验（账号名+密码）
+import { ref } from "vue";
+
+//1.准备表单对象
+const form = ref({
+  account: "",
+  password: "",
+});
+//2.准备规则对象
+const rules = {
+  account: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
+  password: [
+    { required: true, message: "密码不能为空", trigger: "blur" },
+    { min: 6, max: 14, message: "密码应为6~14个字符", trigger: "blur" },
+  ],
+};
 </script>
 
 
@@ -23,12 +39,18 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form
+              :model="form"
+              :rules="rules"
+              label-position="right"
+              label-width="60px"
+              status-icon
+            >
+              <el-form-item label="账户" prop="account">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
