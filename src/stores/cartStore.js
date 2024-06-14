673b7325d2a9ddfffdef49f50dc9c82de7fd0ebe
LY <1481813890@qq.com>
2024-06-14 14:52:33 +0800

    本地购物车-头部购物车计算属性

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,6 +1,6 @@
 //封装购物车模块
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export const useCartStore = defineStore('cart', () => {
   //1.定义state-cartList
@@ -29,10 +29,19 @@ export const useCartStore = defineStore('cart', () => {
     cartList.value.splice(idx, 1)
 
   }
+
+  //计算属性
+  //1.总的数量 所有项的count之和
+  const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+  //2.总价 所有项饿count*price之和
+  const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
   return {
     cartList,
     addCart,
-    delCart
+    delCart,
+    allCount,
+    allPrice
   }
 }, {
   persist: true
