a5a04a6ce158266a2983c2637101a5efd970470d
LY <1481813890@qq.com>
2024-06-20 17:04:08 +0800

    本地购物车-购物车列表-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -35,6 +35,11 @@ export const useCartStore = defineStore('cart', () => {
     const item = cartList.value.find((item) => item.skuId === skuId)
     item.selected = selected
   }
+  //全选功能
+  const allCheck = (selected) => {
+    //把cartList中的每一项的selected都设置为当前的全选框状态
+    cartList.value.forEach(item => item.selected = selected)
+  }
 
 
   //计算属性
@@ -43,13 +48,18 @@ export const useCartStore = defineStore('cart', () => {
   //2.总价 所有项饿count*price之和
   const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
 
+
+  //是否全选
+  const isAll = computed(() => cartList.value.every((item) => item.selected))
   return {
     cartList,
     addCart,
     delCart,
     allCount,
     allPrice,
-    singleCheck
+    singleCheck,
+    isAll,
+    allCheck
   }
 }, {
   persist: true
