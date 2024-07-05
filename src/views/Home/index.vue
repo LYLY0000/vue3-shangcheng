c54f9320d8f514a64f1b42f30b5fc67d80c98426
LY <1481813890@qq.com>
2024-07-05 20:07:45 +0800

    支付-获取基本支付信息

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,9 @@
 <script setup>
-import HomeCategory from './components/HomeCategory.vue'
-import HomeBanner from './components/HomeBanner.vue'
-import HomeNew from './components/HomeNew.vue'
-import HomeHot from './components/HomeHot.vue'
-import HomeProduct from './components/HomeProduct.vue'
-
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeHot from "./components/HomeHot.vue";
+import HomeProduct from "./components/HomeProduct.vue";
 </script>
 
 <template>
@@ -15,5 +14,4 @@ import HomeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <HomeProduct />
-
 </template>
