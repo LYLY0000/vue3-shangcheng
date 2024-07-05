de1629d0e2edeb0cf1dfb927f6c07cda407f613e
LY <1481813890@qq.com>
2024-07-05 17:01:09 +0800

    切换地址-激活交互实现

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -20,6 +20,16 @@ onMounted(() => {
 });
 
 const showDialog = ref(false);
+
+//切换地址
+const activeAddress = ref({});
+const switchAddress = (item) => {
+  activeAddress.value = item;
+};
+const confirm = () => {
+  curAddress.value = activeAddress.value;
+  showDialog.value = false;
+};
 </script>
 
 <template>
@@ -139,6 +149,8 @@ const showDialog = ref(false);
     <div class="addressWrapper">
       <div
         class="text item"
+        :class="{ active: activeAddress.id === item.id }"
+        @click="switchAddress(item)"
         v-for="item in checkInfo.userAddresses"
         :key="item.id"
       >
@@ -154,7 +166,7 @@ const showDialog = ref(false);
     <template #footer>
       <span class="dialog-footer">
         <el-button>取消</el-button>
-        <el-button type="primary">确定</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
       </span>
     </template>
   </el-dialog>
